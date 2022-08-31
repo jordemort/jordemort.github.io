b428590881db246a3b7c04edbdf3d0a62e2d6947
Jordan Webb <jordan@caketop.app>
2022-08-31 14:10:13 -0500

    GitHub Action to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jordemort.dev',
+	site: 'https://jordemort.github.io',
 	integrations: [mdx(), sitemap()],
 });
