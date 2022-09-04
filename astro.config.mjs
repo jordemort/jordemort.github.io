82b68ebe3edabfa4deaf0fa232dc906fbece6840
Jordan Webb <jordan@caketop.app>
2022-09-04 16:10:44 -0500

    Fix RSS URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jordemort.github.io',
+	site: 'https://jordemort.dev',
 	integrations: [mdx(), sitemap()],
 });
