60c05baddf350872cdb568dd95e71e3fc0233567
Jordan Webb <jordan@caketop.app>
2022-09-06 22:18:38 -0500

    Properly sort RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,30 @@
 import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
+function getRSSPosts() {
+	let posts = Object.values(
+		import.meta.globEager('./blog/**/*.md')
+	).sort((a, b) =>
+		new Date(b.frontmatter.pubDate).valueOf() -
+		new Date(a.frontmatter.pubDate).valueOf()
+	).map(post => ({
+		link: post.url + '/',
+		title: post.frontmatter.title,
+		description: post.frontmatter.description,
+		pubDate: new Date(post.frontmatter.pubDate)
+	}));
+
+	if (posts.length > 10) {
+		posts.length = 10;
+	}
+
+	return posts;
+}
+
 export const get = () =>
 	rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.md'),
+		items: getRSSPosts()
 	});
