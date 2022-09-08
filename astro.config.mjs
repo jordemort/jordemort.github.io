9f3827a7f23381e108d384d37d5c81a782d91339
Jordan Webb <jordan@caketop.app>
2022-09-08 17:30:06 -0500

    CSS tweaks, math + emoji for markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,23 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { repoDatesPlugin } from "./src/plugins/repodates.mjs";
-import { defaultLayoutPlugin } from './src/plugins/defaultlayout.mjs';
-
 import compress from "astro-compress";
 
-// https://astro.build/config
+import { repoDatesPlugin } from "./src/plugins/repodates.mjs";
+import { defaultLayoutPlugin } from './src/plugins/defaultlayout.mjs';
+import remarkGemoji from 'remark-gemoji';
+import remarkMath from 'remark-math';
+
+import rehypeKatex from 'rehype-katex';
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
+
 export default defineConfig({
   site: 'https://jordemort.dev',
-  integrations: [mdx(), sitemap(), compress({html: {removeComments: true}})],
+  integrations: [mdx(), sitemap(), compress({ html: { removeComments: true } })],
   markdown: {
-    remarkPlugins: [repoDatesPlugin, defaultLayoutPlugin],
+    extendDefaultPlugins: true,
+    remarkPlugins: [repoDatesPlugin, defaultLayoutPlugin, remarkGemoji, remarkMath],
+    rehypePlugins: [rehypeKatex, rehypeAccessibleEmojis],
     shikiConfig: {
       theme: "dark-plus"
     }
