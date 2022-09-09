4c2686d6612baa427e0e4dd427e08396b34b585c
Jordan Webb <jordan@caketop.app>
2022-09-08 20:52:54 -0500

    Add mermaidjs diagrams

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,19 +7,36 @@ import { repoDatesPlugin } from "./src/plugins/repodates.mjs";
 import { defaultLayoutPlugin } from './src/plugins/defaultlayout.mjs';
 import remarkGemoji from 'remark-gemoji';
 import remarkMath from 'remark-math';
+import remarkMermaid from 'remark-mermaidjs';
+
+//import mdxMermaidPlugin from '@julian_cataldo/astro-diagram';
 
 import rehypeKatex from 'rehype-katex';
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
+import chromium from 'chromium';
 
 export default defineConfig({
   site: 'https://jordemort.dev',
-  integrations: [mdx(), sitemap(), compress({ html: { removeComments: true } })],
+  integrations: [
+    mdx(),
+    sitemap(),
+    compress({ html: { removeComments: true } })
+  ],
   markdown: {
     extendDefaultPlugins: true,
-    remarkPlugins: [repoDatesPlugin, defaultLayoutPlugin, remarkGemoji, remarkMath],
-    rehypePlugins: [rehypeKatex, rehypeAccessibleEmojis],
+    remarkPlugins: [
+      repoDatesPlugin,
+      defaultLayoutPlugin,
+      remarkGemoji,
+      remarkMath,
+      [remarkMermaid, {launchOptions: {executablePath: chromium.path}}]
+    ],
+    rehypePlugins: [
+      rehypeKatex,
+      rehypeAccessibleEmojis
+    ],
     shikiConfig: {
-      theme: "dark-plus"
+      theme: "github-dark"
     }
   }
 });
